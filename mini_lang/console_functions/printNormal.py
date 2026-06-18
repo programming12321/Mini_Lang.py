4ae5785a5da05d532a8dc615fc3335df1b3e24d3
programming12321 <joaquin.adones.v@colegiosantamariaeufrasia.cl>
2026-06-18 16:45:00 -0400

    Delete mini_lang/console_functions/printNormal.py

--- mini_lang/console_functions/printNormal.py
+++
@@ -1,6 +0,0 @@
-def run_printNormal(code):
-    if not code.endswith(")"):
-        print("SyntaxError: missing parenthesis")
-    else:
-        content = code[len("printNormal("):-1]
-        print(content)
